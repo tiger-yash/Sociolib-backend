69d6bdaa8233212332d147e4febc56fc47aef087
tiger-yash <tigeryashraj@gmail.com>
2021-11-01 12:06:23 +0530

    serializer created

---
+++ friends/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from authentication.models import Account
+
+
+class FriendSerializer(serializers.ModelSerializer):
+    is_self=serializers.BooleanField(default=False)
+    is_friend=serializers.BooleanField(default=False)
+    request_sent=serializers.BooleanField(default=False)
+    request_received=serializers.BooleanField(default=False)
+    class Meta:
+        model=Account
+        fields=('id','username','is_self','is_friend','request_sent','request_received')
