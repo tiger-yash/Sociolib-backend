67934167390e4b7cd63971a081cc44ccd57a75df
tiger-yash <tigeryashraj@gmail.com>
2021-11-02 19:29:52 +0530

    library serializers created

--- library/serializers.py
+++ library/serializers.py
@@ -2,3 +2,18 @@ from rest_framework import serializers
 from authentication.models import Account
 from django.contrib.auth import authenticate
 from rest_framework.validators import UniqueValidator
+from .models import BookInfo,Reviews
+
+class BooksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=BookInfo
+        fields='__all__'
+        depth=2
+
+class ReviewsSerializer(serializers.ModelSerializer):
+    comment = serializers.CharField(max_length=280)
+    rating = serializers.DecimalField(max_digits=2,decimal_places=1)
+    class Meta:
+        model=BookInfo
+        fields=('comment','rating')
+    
--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-# from .views import 
+from .views import BooksView,IssueView,ReviewView
 
 urlpatterns = [
-    # path('login/', LoginView.as_view())
+    path('books/', BooksView.as_view()),
+    path('book/<int:pk>/', BooksView.as_view()),
+    path('issue/<int:pk>/', IssueView.as_view()),
+    path('review/<int:pk>/', ReviewView.as_view()),
 ]
