45a2ab83cd37a61e42a2c9ce849509d86c9156b6
tiger-yash <tigeryashraj@gmail.com>
2021-11-11 00:01:52 +0530

    upd2

--- sociolib_backend/settings.py
+++ sociolib_backend/settings.py
@@ -140,10 +140,6 @@ django_heroku.settings(locals())
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
-]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
