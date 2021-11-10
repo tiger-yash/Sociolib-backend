e1b364fd542d52c13c81e49f7e944ecd223f5321
tiger-yash <tigeryashraj@gmail.com>
2021-11-10 23:36:57 +0530

    upd

--- sociolib_backend/settings.py
+++ sociolib_backend/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
 
 TEMP = os.path.join(BASE_DIR, 'media_cdn/temp')
